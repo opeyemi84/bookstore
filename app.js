cac611671792a58585a88c31bed0a224c8a2257e
Opeyemi <opeadetola@gmail.com>
2022-09-20 12:09:37 -0400

    Imported the book routes into app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
 const { connectdb } = require("./db");
 require("dotenv").config();
+
+const bookRouter = require("./routes/books");
 
 const app = express();
 
@@ -12,10 +15,14 @@ connectdb();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("welcome home");
-});
+app.use("/books", bookRouter);
+
+// app.get("/", (req, res) => {
+//   res.send("welcome home");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: http://localhost: ${PORT}`);
