1f223decdc07cf2be4f342ba7419ae3bfdeb52c4
Opeyemi <opeadetola@gmail.com>
2022-09-20 12:04:13 -0400

    Created routes handler folder

---
+++ routes/books.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const book_controller = require("../controller/booksController");
+
+router.get("/", book_controller.index);
+
+router.get("/:id", book_controller.create_books_id);
+
+router.post("/", book_controller.create_books);
+
+router.put("/:id", book_controller.update_books);
+
+router.delete("/:id", book_controller.delete_books);
+
+module.exports = router;
