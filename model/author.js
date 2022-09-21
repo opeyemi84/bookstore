793c65ba1ddbaf8dbd1e311ee7545d9b94dcafa8
Opeyemi <opeadetola@gmail.com>
2022-09-21 05:46:03 -0400

    Updated to include schema for authors

--- model/author.js
+++ model/author.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const authorSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
+  yearOfBirth: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now() },
   updatedAt: { type: Date, default: Date.now() },
 });
